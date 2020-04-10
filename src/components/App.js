431ed76432df6c2d48e927356973c72fd3a15fee
m-ossolinski <mateusz.ossolinski@gmail.com>
2020-04-10 14:21:57 +0200

    created a basic boilerplate based on jsx

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './index.scss';
+
+export const App = () => (
+  <div className="main">
+    <div>
+      <h1>React-TS-boilerplate</h1>
+    </div>
+    <div>
+      <p>A simple react-boilerplate without using create-react-app.</p>
+      <span>
+        <p>It contains:</p>
+        <ul>
+          <li>Parcel</li>
+          <li>Prettier</li>
+          <li>Eslint</li>
+        </ul>
+      </span>
+      <span className="author">
+        <p>
+          Created by:{' '}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/m-ossolinski"
+          >
+            m-ossolinski
+          </a>
+        </p>
+      </span>
+    </div>
+  </div>
+);
